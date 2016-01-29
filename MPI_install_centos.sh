ae2a9eb1f7f88364b40f2fb00c47535ca9015225
Chengduo Zhao <577712541@qq.com>
2016-01-29 23:09:28 +0800

    Update MPI_install_centos.sh

--- MPI_install_centos.sh
+++ MPI_install_centos.sh
@@ -2,16 +2,16 @@ echo "Downloading mpich-3.2.tar.gz"
 wget http://www.mpich.org/static/downloads/3.2/mpich-3.2.tar.gz
 tar -zxvf mpich-3.2.tar.gz
 cd mpich-3.2
-./configure --enable-fc --enable-cxx --enable-romio --enable-threads=multiple --prefix=/usr/local/mpich
+./configure --prefix=/usr/local/mpich --enable-f90  --enable-cxx --enable-threads=multiple --enable-sharedlibs=gcc --with-mpe --enable-romio --enable-fc
 make -j8
 make install
-
+"
 sudo echo "export PATH=/usr/local/mpich/bin:${PATH}  
 export LD_LIBRARY_PATH=/usr/local/mpich/lib:${LD_LIBRARY_PATH}  
 export MANPATH=/usr/local/mpich/share/man:${MANPATH}" >> profile.txt
 sudo cat profile.txt >> /etc/profile
 sudo rm profile.txt
 sudo source
-
+"
 sudo sh -c 'echo "/usr/local/mpich/lib" > /etc/ld.so.conf.d/mpi.conf'
 sudo ldconfig
